90fadc4e12c470884fa38d7f788fe1959c0a1a66
Ueli Kunz <kunz@ideadapt.net>
2021-12-28 21:10:42 +0100

    day14 part 2. try to adopt mapReduce style algorithm.

--- src/main/kotlin/daiy14/polymer.kt
+++ src/main/kotlin/daiy14/polymer.kt
@@ -1,40 +1,50 @@
 package daiy14
 
-class Production(
-	val input: String,
-	val output: Char,
-	val left: String = input[0].toString() + output,
-	val right: String = output + input[1].toString()
-)
+data class Tuple(val left: String, val right: String, val pair: String = left + right)
+data class TupleCount(val tuple: Tuple, val count: Long)
+
+fun mapReduce(
+	mapper: (input: TupleCount) -> List<TupleCount>,
+	reducer: (key: Tuple, values: List<Long>) -> TupleCount,
+	list: List<TupleCount>
+): List<TupleCount> {
+	val mapped = list.map(mapper).flatten()
+	val shuffled = mapped.groupBy(keySelector = { it.tuple }, valueTransform = { it.count })
+	return shuffled.mapValues { (key, values) -> reducer(key, values) }.values.toList()
+}
 
 fun polymer(lines: List<String>): Long {
 	require(lines.isNotEmpty()) { "lines must not be empty" }
-	val template = lines.first()
-	val rules =
-		lines.drop(2).map { rule -> Production(input = rule.take(2), output = rule.takeLast(1).toCharArray()[0]) }
-	val charCount = mutableMapOf<Char, Long>()
-	var lastRoundPairs = mutableMapOf<String, Long>()
-
-	template.forEach { char ->
-		charCount.compute(char) { _, count -> (count ?: 0) + 1 }
+	val rules = lines.drop(2).associate { rule ->
+		Pair(rule.take(2), rule.takeLast(1))
 	}
+	val template = lines.first()
 
-	template.windowed(2).forEach { pair ->
-		lastRoundPairs.compute(pair) { _, count -> (count ?: 0) + 1 }
+	var tupleCounts = template.windowed(2).map {
+		TupleCount(Tuple(left = it.take(1), right = it.takeLast(1)), 1)
 	}
 
 	(1..10).forEach { _ ->
-		val roundPairs = mutableMapOf<String, Long>()
-		rules.forEach { rule ->
-			if (lastRoundPairs.containsKey(rule.input)) {
-				val nrOfProductions = lastRoundPairs[rule.input]!!
-				roundPairs.compute(rule.left) { _, count -> (count ?: 0) + nrOfProductions }
-				roundPairs.compute(rule.right) { _, count -> (count ?: 0) + nrOfProductions }
-				charCount.compute(rule.output) { _, count -> (count ?: 0) + nrOfProductions }
-			}
-		}
-		lastRoundPairs = roundPairs
+		tupleCounts = mapReduce(
+			mapper = { tupleCount ->
+				val new = rules[tupleCount.tuple.pair]!!
+				val left = TupleCount(Tuple(tupleCount.tuple.left, new), tupleCount.count)
+				val right = TupleCount(Tuple(new, tupleCount.tuple.right), tupleCount.count)
+				listOf(left, right)
+			},
+			reducer = { tuple, values ->
+				TupleCount(tuple, values.sum())
+			},
+			list = tupleCounts
+		)
 	}
+
+	val charCount = tupleCounts
+		.groupBy(keySelector = { it.tuple.left }, valueTransform = { it.count })
+		.mapValues { (_, values) -> values.sum() }
+		.toMutableMap()
+
+	charCount[template.takeLast(1)] = charCount[template.takeLast(1)]!! + 1
 
 	val sorted = charCount.values.sorted()
 	return sorted.last() - sorted.first()
