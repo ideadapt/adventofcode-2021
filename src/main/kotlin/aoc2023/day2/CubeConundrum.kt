f1190985741f9912923b97972d8edf16b30f4de7
Ueli Kunz <kunz@ideadapt.net>
2023-12-03 21:22:53 +0100

    refactor

--- src/main/kotlin/aoc2023/day2/CubeConundrum.kt
+++ src/main/kotlin/aoc2023/day2/CubeConundrum.kt
@@ -1,13 +1,15 @@
 package aoc2023.day2
 
+fun Regex.firstGroupInt(str: String): Sequence<Int> = this.findAll(str).map { it.groupValues[1].toInt() }
+
 class CubeConundrum {
 
     fun validGamesSum(games: List<String>, red: Int, green: Int, blue: Int): Int {
         return games
             .mapIndexedNotNull { index, game ->
-                val blues = Regex("(\\d+) blue").findAll(game).map { it.groupValues[1].toInt() }
-                val reds = Regex("(\\d+) red").findAll(game).map { it.groupValues[1].toInt() }
-                val greens = Regex("(\\d+) green").findAll(game).map { it.groupValues[1].toInt() }
+                val blues = Regex("(\\d+) blue").firstGroupInt(game)
+                val reds = Regex("(\\d+) red").firstGroupInt(game)
+                val greens = Regex("(\\d+) green").firstGroupInt(game)
 
                 if (blues.all { it <= blue } && reds.all { it <= red } && greens.all { it <= green })
                     index
@@ -19,9 +21,9 @@ class CubeConundrum {
 
     fun validGamesPowerSum(games: List<String>): Int {
         return games.sumOf { game ->
-            val blues = Regex("(\\d+) blue").findAll(game).map { it.groupValues[1].toInt() }
-            val reds = Regex("(\\d+) red").findAll(game).map { it.groupValues[1].toInt() }
-            val greens = Regex("(\\d+) green").findAll(game).map { it.groupValues[1].toInt() }
+            val blues = Regex("(\\d+) blue").firstGroupInt(game)
+            val reds = Regex("(\\d+) red").firstGroupInt(game)
+            val greens = Regex("(\\d+) green").firstGroupInt(game)
 
             blues.max() * reds.max() * greens.max()
         }
